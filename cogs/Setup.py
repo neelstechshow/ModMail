ba4833e7e1799d03f0b16e679d1f2a75a88b6819
Programming-Done-Right <84484122+Programming-Done-Right@users.noreply.github.com>
2021-06-30 01:13:54 -0700

    Create Setup.py

---
+++ cogs/Setup.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+
+class Startup(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('We have logged in as {0.user}'.format(self.bot))
+        await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Over ModMail"))
+
+def setup(bot):
+    bot.add_cog(Startup(bot))
+    
+
+
+
