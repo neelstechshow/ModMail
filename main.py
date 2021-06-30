bf74207465625986a303299f083a0e00f05d7fab
Programming-Done-Right <84484122+Programming-Done-Right@users.noreply.github.com>
2021-06-30 01:13:32 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+import discord
+from discord.ext import commands
+import os
+from keep_alive import keep_alive
+
+intents = discord.Intents().all()
+bot = commands.Bot(command_prefix='?', intents=intents )
+
+@bot.command()
+async def load(ctx, extension):
+    bot.load_extension(f'cogs.{extension[:-3]}')
+
+
+@bot.command()
+async def unload(ctx, extension):
+    bot.unload_extension(f'cogs.{extension}')
+
+@bot.command()
+async def reload(ctx, extension):
+    bot.unload_extension(f'cogs.{extension}')
+    bot.load_extension(f'cogs.{extension}')
+
+for filename in os.listdir('cogs'):
+    if filename.endswith('.py'):
+        bot.load_extension(f'cogs.{filename[:-3]}')
+
+keep_alive()
+token = os.environ.get('Token')
+bot.run(token)
