490fc883c694ae372fabf791e3e18cc0f4c18941
Programming-Done-Right <84484122+Programming-Done-Right@users.noreply.github.com>
2021-06-30 01:25:59 -0700

    Update Mod.py

--- cogs/Mod.py
+++ cogs/Mod.py
@@ -31,8 +31,8 @@ class Member(commands.Cog):
     async def closeTicket(self, ctx):
         try:
             user = discord.utils.get(self.bot.get_all_members(), id=int(ctx.channel.name))
-            embedVar = discord.Embed(title='The Moderators Have Closed This Thread' , description='', color=0x00ff00) # Blue: 0x0000ff, Red: 0xff0000
-            await user.send(embed=embedVar)
+            embedVariable = discord.Embed(title='The Moderators Have Closed This Thread' , description='', color=0x00ff00) # Blue: 0x0000ff, Red: 0xff0000
+            await user.send(embed=embedVariable)
             await ctx.channel.delete()
         except:
             pass
