7d048f78adce6d98482a53de83730e5572a52283
Programming-Done-Right <84484122+Programming-Done-Right@users.noreply.github.com>
2021-06-30 18:15:13 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,18 +4,21 @@ import os
 from keep_alive import keep_alive
 
 intents = discord.Intents().all()
-bot = commands.Bot(command_prefix='?', intents=intents )
+bot = commands.Bot(command_prefix='>', intents=intents )
 
 @bot.command()
+@commands.has_permissions(administrator=True)
 async def load(ctx, extension):
     bot.load_extension(f'cogs.{extension[:-3]}')
 
 
 @bot.command()
+@commands.has_permissions(administrator=True)
 async def unload(ctx, extension):
     bot.unload_extension(f'cogs.{extension}')
 
 @bot.command()
+@commands.has_permissions(administrator=True)
 async def reload(ctx, extension):
     bot.unload_extension(f'cogs.{extension}')
     bot.load_extension(f'cogs.{extension}')
